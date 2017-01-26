bd2128e3b0232e7dfd477c6e4943b1a3db149459
rachelkahn08 <Rachel@Rachels-MacBook-Pro-2.local>
2017-01-26 16:53:09 -0500

    Initial
    welp.

---
+++ week-2/assets/js/main.js
@@ -0,0 +1,11 @@
+// kabob
+
+// railroad
+	// build R with CSS
+	// flip R for perspective
+	// build train
+		// engine
+		// caboose
+	// train moves around r
+		// whole circle rotates
+		// wheels spin perpendicular
